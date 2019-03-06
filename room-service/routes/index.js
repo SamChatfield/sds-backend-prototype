cd5e4f43a6dfd2287530d08e609e5a91704c30c5
Sam Chatfield <samchatfield97@gmail.com>
2019-03-06 17:39:05 +0000

    Incorporate door-auth functionality into this. studentId body parameter has changed to userId to match the schema of the users.

--- room-service/routes/index.js
+++ room-service/routes/index.js
@@ -2,8 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
+const roomPermissions = {
+  222: ['1642203'],
+  225: ['1234567'],
+};
+
 router.get('/', (req, res) => {
   res.send('Room Service');
+});
+
+router.post('/:roomNumber/unlock', (req, res) => {
+  const { roomNumber } = req.params;
+
+  if (Object.keys(roomPermissions).includes(roomNumber)) {
+    if (req.body.userId) {
+      if (roomPermissions[roomNumber].includes(req.body.userId)) {
+        res.json({ unlock: true });
+      } else {
+        res.json({ unlock: false });
+      }
+    } else {
+      res.status(400).json({ error: 'No userId provided' });
+    }
+  } else {
+    res.status(400).json({ error: 'Invalid room number provided' });
+  }
 });
 
 module.exports = router;
