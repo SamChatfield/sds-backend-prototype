97da56e845a82f7308f2b76d9b5ce2ac9e6e4412
Sam Chatfield <samchatfield97@gmail.com>
2019-03-06 12:27:50 +0000

    Add CORS config to server.

--- user-service/app.js
+++ user-service/app.js
@@ -4,12 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-// Database setup
+// Database setup (2 second delay to allow for DB service to start)
 setTimeout(() => {
   mongoose.connect('mongodb://db/user?authSource=admin', {
     useNewUrlParser: true,
@@ -25,6 +26,7 @@ setTimeout(() => {
   });
 }, 2000);
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
