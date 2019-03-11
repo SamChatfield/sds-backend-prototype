31df6353265c2885ba737dca9903418a0f6c4a7c
Sam Chatfield <samchatfield97@gmail.com>
2019-03-11 17:55:39 +0000

    Modify database config. Move URI to .env file. Remove timeout now that the DB is hosted on mLab and should always be running on startup.

--- user-service/app.js
+++ user-service/app.js
@@ -10,22 +10,18 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-// Database setup (2 second delay to allow for DB service to start)
-setTimeout(() => {
-  // mongoose.connect('mongodb://db/user?authSource=admin', {
-  mongoose.connect('mongodb://ds159185.mlab.com:59185/sds', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-  }).then(() => {
-    console.log('Connected to database');
-  }).catch((err) => {
-    console.error(`Could not connect to database. Exiting...\n${err}`);
-    process.exit(1);
-  });
-}, 2000);
+mongoose.connect(`mongodb://${process.env.DB_URI}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+}).then(() => {
+  console.log('Connected to database');
+}).catch((err) => {
+  console.error(`Could not connect to database. Exiting...\n${err}`);
+  process.exit(1);
+});
 
 app.use(cors());
 app.use(logger('dev'));
