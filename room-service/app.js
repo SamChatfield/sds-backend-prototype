1976d0bf0625e99314c10f9e4ab768c385a2d2a5
Sam Chatfield <samchatfield97@gmail.com>
2019-03-12 11:48:21 +0000

    Add requirements for connecting to database and using cors headers to room-service.

--- room-service/app.js
+++ room-service/app.js
@@ -1,12 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+mongoose.connect(`mongodb://${process.env.DB_URI}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+}).then(() => {
+  console.log('Connected to database');
+}).catch((err) => {
+  console.error(`Could not connect to database. Exiting...\n${err}`);
+  process.exit(1);
+});
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
